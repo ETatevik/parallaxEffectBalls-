8292521a5a7ca2e45d65efd732cf6a13faacca60
eTatev <barev.tatevik@gmail.com>
2019-12-13 22:53:40 +0400

    Right Ball move

--- js/parallax.js
+++ js/parallax.js
@@ -2,10 +2,18 @@ jQuery(document).ready(function($) {
 	
 	{
 		const parent = $('header.parallax-Parent');
+
 		const plBg = $('.parallax > .bg-parallex-et');
 		let plBg_X = parseFloat(plBg.css('left'));
 		let plBg_Y = parseFloat(plBg.css('top'));
-		let step = 1;
+
+		const ball_left = $('.box-left-et');
+		let left_X = parseFloat(ball_left.css('left'));
+		let left_Y = parseFloat(ball_left.css('top'));
+
+		const ball_right = $('.box-right-et');
+		let right_X = parseFloat(ball_right.css('right'));
+		let right_Y = parseFloat(ball_right.css('top'));
 
 		parent.mousemove(function(e) {
 			let widthP = $(this).width();
@@ -20,16 +28,35 @@ jQuery(document).ready(function($) {
 			}
 
 			if(x + plBg.width() >= widthP + plBg.width() / 4){
-				plBg_X = widthP - (plBg.width() - 50);
+				plBg_X = widthP - (plBg.width() - 100);
 			}
 
 			if( y + plBg.height() >= heightP +plBg.height() / 4){
-				plBg_Y = heightP - (plBg.height() - 50);
+				plBg_Y = heightP - (plBg.height() - 100);
+			}
+
+			if( (x >= 0 && x <= widthP/2)){
+				left_X = -30;
+			}
+			if( (x > widthP/2 && x <= widthP)){
+				left_X = -5;
+			}
+			
+			if( (y >= 0 && y <= heightP/2)){
+				left_Y = -50;
+			}
+			if( (y > heightP/2 && y <= heightP)){
+				left_Y = 20;
 			}
 
 			plBg.css({
 				left: `${plBg_X}px`,
 				top: `${plBg_Y}px`
+			});
+
+			ball_left.css({
+				left: `${left_X}%`,
+				top: `${left_Y}%`
 			});
 
 		});
